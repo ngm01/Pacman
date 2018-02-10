f2dd49c2ac3bf5c9906024e367308a4bc6d2e1a9
ngm01 <nathaniel.g.moore@gmail.com>
2018-02-09 20:43:44 -0500

    still debugging...

--- game.js
+++ game.js
@@ -79,64 +79,72 @@
     
     displayScore();
 
-	function generateMaze(){
-		var maze = [];
-		for(let i=0;i<10;i++){
-			if(i==0 || i==9){
-				maze.push(outerWall());
-			}
-			else{
-				let newRow = [];
-				for(let k=0;k<10;k++){
-					if(k==0 || k==9){
-						newRow.push(2);
-					}
-					else{
-						newRow.push(1);
-					}
-				}
-				maze.push(newRow);
-			}
+    function generateMaze(){
+        let maze = [];
+        for(let i=0;i<10;i++){
+            if(i==0 || i==9){
+                maze.push(outerWall());
+            }
+            else{
+                let newRow = [];
+                for(let k=0;k<10;k++){
+                    if(k==0 || k==9){
+                        newRow.push(2);
+                    }
+                    else{
+                        newRow.push(1);
+                    }
+                }
+                maze.push(newRow);
+            }
         }
         maze = buildInnerWalls(maze);
-		maze[1][1] = 0;
-		return maze;
-	}
-
-	function outerWall(){
-		var row = []
-		for(let i=0;i<10;i++){
-			row.push(2);
-		}
-		return row;
-	}
-
-	function buildInnerWalls(currentMaze){
+        maze[1][1] = 0;
+        return maze;
+    }
+    
+    function outerWall(){
+        var row = []
+        for(let i=0;i<10;i++){
+            row.push(2);
+        }
+        return row;
+    }
+    
+    function buildInnerWalls(currentMaze){
         // how many walls?
         // to begin the experiment, let's build five.
         for(let w=0;w<5;w++){
+            console.log("Building wall number:", w + 1);
             buildWall(currentMaze);
         }
-		return currentMaze;
+        return currentMaze;
     }
     
     function buildWall(currentMaze){
         //pick a direction:
-        var mazeSize = currentMaze.length - 2;
-        var direction = randomizer(2);
-        var initialSquare = [randomizer(mazeSize) + 1, randomizer(mazeSize) + 1];
+        const mazeSize = currentMaze.length - 2;
+        const direction = randomizer(2);
+        const initialSquare = [randomizer(mazeSize) + 1, randomizer(mazeSize) + 1];
+        console.log("inital square", initialSquare)
         maxDistanceFirst = randomizer(mazeSize);
+        let pivotPoint = [0, 0];
         if(direction==1){
-            var pivotPoint = verticalBlock(currentMaze, initialSquare);
+            console.log("Pivot point before assignment: " + pivotPoint);
+            pivotPoint = verticalBlock(currentMaze, initialSquare);
+            console.log("Pivot point from vertical? " + pivotPoint);
         }
         else{
+            console.log("Pivot point before assignment: " + pivotPoint);
             pivotPoint = horizontalBlock(currentMaze, initialSquare);
+            console.log("Pivot point from horizontal? " + pivotPoint);
         }
-
+    
         //then turn:
-        var turn = Math.floor(Math.random() * 2);
+        const turn = Math.floor(Math.random() * 2);
         maxDistanceNext = randomizer(mazeSize);
         if(turn==1){
+            console.log("Turning...")
             if(direction==1){
                 horizontalBlock(currentMaze, pivotPoint);
             }
@@ -145,29 +153,29 @@
             }
         }
     }
-
+    
     function verticalBlock(currentMaze, startAt){
+        console.log("Building vertical block: " + startAt);
         if(currentMaze[startAt[0] + 1][startAt[1]] != 2){
             currentMaze[startAt[0][startAt[1]]] = 2;
             startAt[0]++;
             verticalBlock(currentMaze, startAt);
         }
-        else{
+            console.log("Returning start at from vertical: " + startAt);
             return startAt;
-        }
     }
-
+    
     function horizontalBlock(currentMaze, startAt){
+        console.log("Building horizontal block: " + startAt);
         if(currentMaze[startAt[0]][startAt[1] + 1] != 2){
             currentMaze[startAt[0][startAt[1]]] = 2;
             startAt[1]++;
             horizontalBlock(currentMaze, startAt);
         }
-        else{
+            console.log("Returning start at from horizontal: " + startAt);
             return startAt;
-        }
     }
-
+    
     function randomizer(max){
         return Math.floor(Math.random() * max)
     }
--- mazeMaker.js
+++ mazeMaker.js
@@ -36,67 +36,74 @@ function buildInnerWalls(currentMaze){
     for(let w=0;w<5;w++){
         console.log("Building wall number:", w + 1);
         buildWall(currentMaze);
+        mazeLogger(currentMaze);
     }
     return currentMaze;
-}
 
-function buildWall(currentMaze){
-    //pick a direction:
-    const mazeSize = currentMaze.length - 2;
-    const direction = randomizer(2);
-    const initialSquare = [randomizer(mazeSize) + 1, randomizer(mazeSize) + 1];
-    console.log("inital square", initialSquare)
-    maxDistanceFirst = randomizer(mazeSize);
-    let pivotPoint = [0, 0];
-    if(direction==1){
-        console.log("Pivot point before assignment: " + pivotPoint);
-        pivotPoint = verticalBlock(currentMaze, initialSquare);
-        console.log("Pivot point from vertical? " + pivotPoint);
-    }
-    else{
-        console.log("Pivot point before assignment: " + pivotPoint);
-        pivotPoint = horizontalBlock(currentMaze, initialSquare);
-        console.log("Pivot point from horizontal? " + pivotPoint);
-    }
-
-    //then turn:
-    const turn = Math.floor(Math.random() * 2);
-    maxDistanceNext = randomizer(mazeSize);
-    if(turn==1){
-        console.log("Turning...")
+    function buildWall(currentMaze){
+        //pick a direction:
+        const mazeSize = currentMaze.length - 2;
+        const direction = randomizer(2);
+        const initialSquare = [randomizer(mazeSize) + 1, randomizer(mazeSize) + 1];
+        let maxDistanceFirst = randomizer(mazeSize);
+        let pivotPoint = [];
         if(direction==1){
-            horizontalBlock(currentMaze, pivotPoint);
+            pivotPoint = verticalBlock(currentMaze, initialSquare);
         }
         else{
-            verticalBlock(currentMaze, pivotPoint);
+            pivotPoint = horizontalBlock(currentMaze, initialSquare);
         }
+    
+        //then turn:
+        const turn = Math.floor(Math.random() * 2);
+        maxDistanceNext = randomizer(mazeSize);
+        if(turn==1){
+            console.log("Turning...")
+            if(direction==1){
+                horizontalBlock(currentMaze, pivotPoint);
+            }
+            else{
+                verticalBlock(currentMaze, pivotPoint);
+            }
+        }
+        return currentMaze;
+    
+        function verticalBlock(currentMaze, startAt){
+            if(currentMaze[startAt[0] + 1][startAt[1]] == 2){
+                return startAt;
+            }
+            else{
+                currentMaze[startAt[0][startAt[1]]] = 2;
+                startAt[0]++;
+                return verticalBlock(currentMaze, startAt);
+            }
+        }
+        
+        function horizontalBlock(currentMaze, startAt){
+            if(currentMaze[startAt[0]][startAt[1] + 1] == 2){
+                return startAt;
+            }
+            else{
+                currentMaze[startAt[0][startAt[1]]] = 2;
+                startAt[1]++;
+                return horizontalBlock(currentMaze, startAt);
+            }
+        }
+    
     }
 }
 
-function verticalBlock(currentMaze, startAt){
-    console.log("Building vertical block: " + startAt);
-    if(currentMaze[startAt[0] + 1][startAt[1]] != 2){
-        currentMaze[startAt[0][startAt[1]]] = 2;
-        startAt[0]++;
-        verticalBlock(currentMaze, startAt);
-    }
-        console.log("Returning start at from vertical: " + startAt);
-        return startAt;
-}
-
-function horizontalBlock(currentMaze, startAt){
-    console.log("Building horizontal block: " + startAt);
-    if(currentMaze[startAt[0]][startAt[1] + 1] != 2){
-        currentMaze[startAt[0][startAt[1]]] = 2;
-        startAt[1]++;
-        horizontalBlock(currentMaze, startAt);
-    }
-        console.log("Returning start at from horizontal: " + startAt);
-        return startAt;
-}
 
 function randomizer(max){
     return Math.floor(Math.random() * max)
 }
 
-console.log(generateMaze());
+function mazeLogger(someMaze){
+    for(let r=0;r<someMaze.length;r++){
+        console.log(someMaze[r]);
+    }
+}
+
+let myMaze = generateMaze();
+console.log("\nFinal result:\n")
+mazeLogger(myMaze);
